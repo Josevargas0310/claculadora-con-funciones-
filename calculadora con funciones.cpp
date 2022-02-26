f479023820a9c0b3eecd1fc981e03b14882ff91c
josevargas0310 <jvargash1@miumg.edu.gt>
2022-02-26 09:50:48 -0600

    se esta realizando la primera parte que es el menu de la calculadora, donde solo se lleva las funciones para que el usuario escoja

---
+++ calculadora con funciones.cpp
@@ -0,0 +1,17 @@
+#incluide <iostream>
+
+int main()
+(
+ menu(); //se le llama a la funcion con el nombre de menu 
+ return 0;
+)
+
+void menu(){
+	
+	int opcion; //declaramos la varibale que usaremos en el menu 
+	printf("a continuacion digite el numero correspondiente a la funcion aritmatica que desea operar.");
+	printf("\n 1.sumar ");
+	printf("\n 2.restar ");
+	printf("\n 3.multiplicacion");
+	printf("\n 4.division");
+}
