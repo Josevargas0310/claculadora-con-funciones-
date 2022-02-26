f2daf2ab9f00dae876a5832c255d53ace229d110
josevargas0310 <jvargash1@miumg.edu.gt>
2022-02-26 10:06:56 -0600

    se genera las funciones que se van a utilizar en la claculadora y se termino el menu

--- calculadora con funciones.cpp
+++ calculadora con funciones.cpp
@@ -1,5 +1,13 @@
 #incluide <iostream>
 
+//funciones
+void menu();
+void sumar (int num_a, num_b);
+void resta (int num_a, num_b);
+void multiplicacion (int num_a, num_b);
+void division (int num_a, num_b);
+
+
 int main()
 (
  menu(); //se le llama a la funcion con el nombre de menu 
@@ -14,4 +22,27 @@ void menu(){
 	printf("\n 2.restar ");
 	printf("\n 3.multiplicacion");
 	printf("\n 4.division");
+	
+	
+	printf("\n Digite la opcion deseada");
+	scanf ("%i", & num_a);
+	
+	printf("\n Digite la opcion deseada");
+	scanf ("%i", & num_b);
+	
+	swicht (opc)
+	{
+		case 1: sumar (num_a, num_b);
+		break; 
+		case 2: restar (num_a, num_b);
+		break; 
+		case 3: multiplicar (num_a, num_b);
+		break; 
+		case 4: dividir (num_a, num_b);
+		break; 
+		default:
+		printf("opcion invalida")
+	}
+	
+	
 }
